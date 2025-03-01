de64b36ad4cb02f573400f798420e41097363bd8
Lin <lxxer1204@gmail.com>
2025-03-01 13:26:27 -0800

    Add files via upload

---
+++ lab1_ML.py
@@ -0,0 +1,94 @@
+from airflow import DAG
+from airflow.models import Variable
+from airflow.decorators import task
+
+from datetime import timedelta
+from datetime import datetime
+from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
+import requests
+
+
+def return_snowflake_conn():
+    hook = SnowflakeHook(snowflake_conn_id='snowflake_conn')
+    conn = hook.get_conn()
+    return conn.cursor()
+
+
+@task
+def train(cur, train_input_table, train_view, forecast_function_name):
+    """
+     - Create a view with training related columns
+     - Create a model with the view above
+    """
+
+    create_view_sql = f"""CREATE OR REPLACE VIEW {train_view} AS SELECT
+        DATE, CLOSE, SYMBOL
+        FROM {train_input_table};"""
+
+    create_model_sql = f"""CREATE OR REPLACE SNOWFLAKE.ML.FORECAST {forecast_function_name} (
+        INPUT_DATA => SYSTEM$REFERENCE('VIEW', '{train_view}'),
+        SERIES_COLNAME => 'SYMBOL',
+        TIMESTAMP_COLNAME => 'DATE',
+        TARGET_COLNAME => 'CLOSE',
+        CONFIG_OBJECT => {{ 'ON_ERROR': 'SKIP' }}
+    );"""
+
+    try:
+        cur.execute(create_view_sql)
+        cur.execute(create_model_sql)
+        # Inspect the accuracy metrics of the model. 
+        cur.execute(f"CALL {forecast_function_name}!SHOW_EVALUATION_METRICS();")
+    except Exception as e:
+        print(e)
+        raise
+
+
+@task
+def predict(cur, forecast_function_name, train_input_table, forecast_table, final_table):
+    """
+     - Generate predictions and store the results to a table named forecast_table.
+     - Union the predictions with historical data, then create the final table
+    """
+    make_prediction_sql = f"""BEGIN
+        -- This is the step that creates predictions.
+        CALL {forecast_function_name}!FORECAST(
+            FORECASTING_PERIODS => 7,
+            -- Here we set your prediction interval.
+            CONFIG_OBJECT => {{'prediction_interval': 0.95}}
+        );
+        -- These steps store the predictions to a table.
+        LET x := SQLID;
+        CREATE OR REPLACE TABLE {forecast_table} AS SELECT * FROM TABLE(RESULT_SCAN(:x));
+    END;"""
+    create_final_table_sql = f"""CREATE OR REPLACE TABLE {final_table} AS
+        SELECT SYMBOL, DATE, CLOSE AS actual, NULL AS forecast, NULL AS lower_bound, NULL AS upper_bound
+        FROM {train_input_table}
+        UNION ALL
+        SELECT replace(series, '"', '') as SYMBOL, ts as DATE, NULL AS actual, forecast, lower_bound, upper_bound
+        FROM {forecast_table};"""
+
+    try:
+        cur.execute(make_prediction_sql)
+        cur.execute(create_final_table_sql)
+    except Exception as e:
+        print(e)
+        raise
+
+
+with DAG(
+    dag_id = 'StockForecasting',
+    start_date = datetime(2025,2,21),
+    catchup=False,
+    tags=['ML', 'ELT'],
+    schedule = '30 2 * * *'
+) as dag:
+
+    train_input_table = "dev.raw.stock_prices"
+    train_view = "dev.adhoc.stock_prices_view"
+    forecast_table = "dev.adhoc.stock_forecast"
+    forecast_function_name = "dev.analytics.predict_stock_price"
+    final_table = "dev.analytics.stock_prices_forecast"
+    cur = return_snowflake_conn()
+
+    train(cur, train_input_table, train_view, forecast_function_name)
+    predict(cur, forecast_function_name, train_input_table, forecast_table, final_table)
